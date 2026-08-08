4b695c2ed08afe0a5e65025d020d88472c809f0e
brighamdent <brighamdent@gmail.com>
2026-08-08 00:49:29 -0500

    fix: removed rofi config

--- scripts/change-wallpaper.sh
+++ scripts/change-wallpaper.sh
@@ -1,11 +1,9 @@
 #!/bin/sh
 # Defining the path to the Rofi theme file
 rofi_theme_file="$HOME/.config/rofi/config.rasi"
-rofi_power_theme="$HOME/.config/rofi/powermenu.rasi"
 
 # Using sed to replace the background-image path within the inputbar section of the .rasi (rofi) file
 sed -i "/inputbar {/,/}/ s|url(\"[^\"]*\", width)|url(\"$1\", width)|" "$rofi_theme_file"
-sed -i "/inputbar {/,/}/ s|url(\"[^\"]*\", width)|url(\"$1\", width)|" "$rofi_power_theme"
 
 #Changes background using swww
 swww img --transition-type wipe --transition-angle 30 --transition-step 200 "$1"
