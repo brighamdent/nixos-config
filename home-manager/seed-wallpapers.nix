9e9c5cb315ef22ba01285cc832476401eb5591f9
brighamdent <brighamdent@gmail.com>
2026-08-08 02:32:07 -0500

    fix: seed wallpaper

--- home-manager/seed-wallpapers.nix
+++ home-manager/seed-wallpapers.nix
@@ -1,10 +1,17 @@
 { pkgs, lib, ... }:
 {
-  home.activation.seedDefaultWallpaper = lib.hm.dag.entryAfter [ "cloneWallpapers" ] ''
+  home.activation.seedDefaultWallpaper = lib.hm.dag.entryAfter [ "writeBoundary" ] ''
     if [ ! -f "$HOME/.current_wallpaper/currentwallpaper.png" ]; then
       default_wall="$HOME/media/wallpapers/default.png"
       if [ -f "$default_wall" ]; then
-        $DRY_RUN_CMD ${pkgs.change-wallpaper}/bin/change-wallpaper "$default_wall"
+        mkdir -p "$HOME/.current_wallpaper"
+        $DRY_RUN_CMD ${pkgs.pywal}/bin/wal -i "$default_wall" -n || true
+        $DRY_RUN_CMD ${pkgs.imagemagick}/bin/magick convert "$default_wall" \
+          "$HOME/.current_wallpaper/currentwallpaper.png" || true
+        $DRY_RUN_CMD ${pkgs.imagemagick}/bin/magick convert \
+          "$HOME/.current_wallpaper/currentwallpaper.png" \
+          -scale 10% -blur 0x4 -resize 1000% \
+          "$HOME/.current_wallpaper/currentwallpaperblurred.png" || true
       fi
     fi
   '';
